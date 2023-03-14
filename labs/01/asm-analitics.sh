03acc4c6bbb9afa748f02e43e2ac3c5b470b21a4
danialex321 <38055558+danialex321@users.noreply.github.com>
2023-03-13 20:45:52 -0600

    A01566831-homework1

--- labs/01/asm-analitics.sh
+++ labs/01/asm-analitics.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/bash
 
-echo 'hi, this is done :)'
 
-echo 'mistake'
+echo 'hi, start of the program'
+
+echo 'the number of lines in simple_foo file are:'
+objdump -d ./simple_foo | wc -l
+
+echo 'the number of lines with instructions are:'
+objdump -d ./simple_foo | grep -hP '^[ \t]+[a-z]*'| sed 's/^[ \t]\+\([a-z]*\).*/\1/' | sort | uniq -c
+
+echo 'the number of mov'
+objdump -d ./simple_foo | grep -wo mov | wc -w
+
+echo 'list of assembly instructions'
+objdump -d ./simple_foo |grep ^\ |expand|cut -c41-|sed 's/ .*//'|sort|uniq -c|sort -n -r |head
