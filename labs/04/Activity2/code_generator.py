2c736775ff1b394c432b01c14e90e09288a05c89
joalgueflo <114319054+joalgueflo@users.noreply.github.com>
2024-01-21 13:51:37 -0600

    A01285612_JoseGuerrero_Act2

--- labs/04/Activity2/code_generator.py
+++ labs/04/Activity2/code_generator.py
@@ -2,6 +2,7 @@ import string
 import random
 import argparse
 
+
 def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
